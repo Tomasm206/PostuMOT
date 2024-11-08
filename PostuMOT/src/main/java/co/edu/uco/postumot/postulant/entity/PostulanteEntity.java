1ea82f8c88d1723fdbbaa417cb67f1233c745f58
Tomás Marín <79349359+Tomasm206@users.noreply.github.com>
2024-11-08 15:16:54 -0500

    fix: Configuracion al Postgresql
    se configura la conexion al postgresql conla tabla de interés postulante

--- PostuMOT/src/main/java/co/edu/uco/postumot/postulant/entity/PostulanteEntity.java
+++ PostuMOT/src/main/java/co/edu/uco/postumot/postulant/entity/PostulanteEntity.java
@@ -9,20 +9,22 @@ import co.edu.uco.postumot.common.domain.DomainEntity;
 import co.edu.uco.postumot.geografias.entity.CityEntity;
 
 public class PostulanteEntity extends DomainEntity {
-	
-	 private String firstName;
-	 private String secondName;
-	 private String lastName;
-	 private String lastSecondName;
-	 private int phone;
-	 private String email;
-	 private String sex;
-	 private TipoDocumentoEntity tipoDocumento;
-	 private CityEntity city;
-	
+
+	private int documento;
+	private String firstName;
+	private String secondName;
+	private String lastName;
+	private String lastSecondName;
+	private int phone;
+	private String email;
+	private String sex;
+	private TipoDocumentoEntity tipoDocumento;
+	private CityEntity city;
+
 	protected PostulanteEntity() {
 		super(UUIDHelper.getDefault());
 		setTipoDocumento(tipoDocumento);
+		setDocumento(documento);
 		setFirstName(firstName);
 		setSecondName(secondName);
 		setLastName(lastName);
@@ -33,46 +35,59 @@ public class PostulanteEntity extends DomainEntity {
 		setTipoDocumento(tipoDocumento);
 		setCity(city);
 	}
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(final String firstName) {
 		this.firstName = TextHelper.applyTrim(firstName);
 	}
+
 	public String getSecondName() {
 		return secondName;
 	}
+
 	public void setSecondName(final String secondName) {
 		this.secondName = TextHelper.applyTrim(secondName);
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(final String lastName) {
 		this.lastName = TextHelper.applyTrim(lastName);
 	}
+
 	public String getLastSecondName() {
 		return lastSecondName;
 	}
+
 	public void setLastSecondName(final String lastSecondName) {
 		this.lastSecondName = TextHelper.applyTrim(lastSecondName);
 	}
+
 	public long getPhone() {
 		return phone;
 	}
+
 	public void setPhone(final int phone) {
 		this.phone = phone;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(final String email) {
 		this.email = email;
 	}
+
 	public String getSex() {
 		return sex;
 	}
+
 	public void setSex(final String sex) {
 		this.sex = TextHelper.applyTrim(sex);
 	}
@@ -84,9 +99,11 @@ public class PostulanteEntity extends DomainEntity {
 	public void setCity(final CityEntity city) {
 		this.city = ObjectHelper.getDefault(city, new CityEntity());
 	}
+
 	public TipoDocumentoEntity getTipoDocumento() {
 		return tipoDocumento;
 	}
+
 	public void setTipoDocumento(final TipoDocumentoEntity tipoDocumento) {
 		this.tipoDocumento = ObjectHelper.getDefault(tipoDocumento, new TipoDocumentoEntity());
 	}
@@ -95,5 +112,13 @@ public class PostulanteEntity extends DomainEntity {
 	public UUID getId() {
 		return super.getId();
 	}
-	
+
+	public int getDocumento() {
+		return documento;
+	}
+
+	public void setDocumento(int documento) {
+		this.documento = documento;
+	}
+
 }
