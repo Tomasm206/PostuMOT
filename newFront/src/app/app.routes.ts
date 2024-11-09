d1c4939278a4ac688e1e2eef4d87c124dcf94b33
Tomás Marín <79349359+Tomasm206@users.noreply.github.com>
2024-11-09 16:11:16 -0500

    fix: errores en front
    Se corrige el acceso del botón registrar

--- newFront/src/app/app.routes.ts
+++ newFront/src/app/app.routes.ts
@@ -1,24 +1,18 @@
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MenuComponent } from './menu/menu.component'; // Cambia el path si es diferente
-import { RegisterComponent } from './register/register.component'; // Cambia el path si es diferente
+import { MenuComponent } from './menu/menu.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [
-    { path: '', component: MenuComponent }, // La página principal (Home)
-    { path: 'register', component: RegisterComponent } // La página de registro
-  ];
+export const routes: Routes = [ // Exporta la constante 'routes'
+  { path: '', component: MenuComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
-//export const routes: Routes = [];
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
-  })
-  export class AppRoutingModule { }
-
-
-
-// Define las rutas aquí
-
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
 
 
