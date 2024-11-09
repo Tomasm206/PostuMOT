8a81bc04c1c5e789016248b46d321e7c043a1c44
estefaoq <140673103+estefaoq@users.noreply.github.com>
2024-11-09 15:10:21 -0500

    implementaciones en el front
    se emplimento el formulario y su estilo de registrar

--- newFront/src/app/app.routes.ts
+++ newFront/src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu.component'; // Cambia el path si es diferente
+import { RegisterComponent } from './register/register.component'; // Cambia el path si es diferente
+
+const routes: Routes = [
+    { path: '', component: MenuComponent }, // La página principal (Home)
+    { path: 'register', component: RegisterComponent } // La página de registro
+  ];
+
+//export const routes: Routes = [];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
+
+
+
+// Define las rutas aquí
+
+
+
