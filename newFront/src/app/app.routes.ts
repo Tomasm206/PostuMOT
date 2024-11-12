0d026c42e53dece914bf788151d314b5829fec45
estefaoq <140673103+estefaoq@users.noreply.github.com>
2024-11-11 22:02:36 -0500

    front
    se modifica el componte de vacante :)

--- newFront/src/app/app.routes.ts
+++ newFront/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { RegisterComponent } from './register/register.component';
+import { VacantesComponent } from './vacantes/vacantes.component';
 
 export const routes: Routes = [ // Exporta la constante 'routes'
   { path: '', component: MenuComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'vacantes', component: VacantesComponent }
 ];
 
 @NgModule({
